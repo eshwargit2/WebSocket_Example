054630c1a2a4de8be71c4be1e70d045a89f7170f
eshwargit2 <msselectronic57@gmail.com>
2025-06-14 14:56:49 +0530

    frontend update

---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+const socket =io('http://localhost:5000/todo');
+
+const todoItemInput = document.getElementById('todoItemInput');
+const addTodo = document.getElementById('addTodo');
+const todolist = document.getElementById('todolist');
+
+
