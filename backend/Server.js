e6a73d61bf666f43366705c9df9e906f3129101d
Soundhareshwaran S.R <210833648+eshwargit1@users.noreply.github.com>
2025-06-14 12:57:14 +0530

    setup

--- backend/Server.js
+++ backend/Server.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const express = require('express');
 const http = require('http');
 const {Server}= require('socket.io');
@@ -15,7 +16,36 @@ const socketIo= new Server(server, {
 
 let todoList=[];
 
+const NameSpace=socketIo.of('/todo');
+NameSpace.on("connection",(socket)=>{
+  console.log('new connection established');
+  
+  socket.emit('update',todoList);
 
+  socket.on('newItem',(item)=>{
+    const todoItem = {id:uuid(), value:item};
+    todoList.push(todoItem);
+    NameSpace.emit("update", todoList);
+  });
+
+  socket.on('updateItem',(itemObj)=>{
+    const index = todoList.findIndex((item)=>item.id===itemObj);
+    if(index!==1){
+        todoList[index].value=itemObj.value;
+         NameSpace.emit("update", todoList);
+    }
+  });
+
+  socket.on('deleteItem',(id)=>{
+    todoList= todoList.filter(x=>x.id!==id);
+      NameSpace.emit("update", todoList);
+  });
+
+  socket.on('disconnect',()=>{
+     console.log('Client Disconnect');
+  })
+
+});
 
 
 server.listen(5000,()=>{
